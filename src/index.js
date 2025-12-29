de0dbc4f22d64d80412e81e1d8e7316cdf18d552
Swapnil Panigrahy <swa.pan.rt23@dypatil.edu>
2025-12-29 19:30:17 +0530

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import LoginPage from './LoginPage';
 import Home from './Home';
 import ReportDetails from './ReportDetails';
-import UpdateReportForm from './UpdateReportForm'; // Add this import
+import UpdateReportForm from './UpdateReportForm';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/report/:id" element={<ReportDetails />} />
-        <Route path="/update-report/:id" element={<UpdateReportForm />} /> {/* Add this route */}
+        <Route path="/update-report/:id" element={<UpdateReportForm />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
-
-
